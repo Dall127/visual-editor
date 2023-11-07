9f3727338787f02eaf38a0c6129d047d0b371093
Dallin Burton <dallin.burton@me.com>
2023-11-06 20:37:31 -0700

    better support dark mode on Toggle buttons

--- lib/toolbar/widgets/buttons/shared/toggle-button.dart
+++ lib/toolbar/widgets/buttons/shared/toggle-button.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../../shared/models/editor-icon-theme.model.dart';
 import '../../toolbar.dart';
 
-// A generic toggle button with styles can be overridden (color and fill).
-// If no callback is provided the button is considered disabled and renders as so.
-// ignore: must_be_immutable
 class ToggleButton extends StatelessWidget {
   final BuildContext context;
   final IconData icon;
@@ -15,7 +12,6 @@ class ToggleButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final double iconSize;
   final EditorIconThemeM? iconTheme;
-  late ThemeData _theme;
 
   ToggleButton({
     required this.context,
@@ -30,10 +26,16 @@ class ToggleButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _theme = Theme.of(context);
+    ThemeData _theme = Theme.of(context);
+    bool isDarkMode = _theme.brightness == Brightness.dark;
+
     final isEnabled = onPressed != null;
-    final iconColor = isEnabled ? _getToggleColor() : _getDisableToggleColor();
-    final fill = isEnabled ? _getFillColor() : _getDisabledFillColor();
+    final iconColor = isEnabled
+        ? _getToggleColor(isDarkMode, _theme)
+        : _getDisableToggleColor(isDarkMode, _theme);
+    final fill = isEnabled
+        ? _getFillColor(isDarkMode, _theme)
+        : _getDisabledFillColor(_theme);
 
     return IconBtn(
       highlightElevation: 0,
@@ -53,23 +55,35 @@ class ToggleButton extends StatelessWidget {
 
   // === UTILS ===
 
-  Color? _getToggleColor() {
+  Color? _getToggleColor(bool isDarkMode, ThemeData theme) {
     if (isToggled == true) {
-      return iconTheme?.iconSelectedColor ?? _theme.primaryIconTheme.color;
+      return iconTheme?.iconSelectedColor ??
+          (isDarkMode
+              ? theme.colorScheme.onPrimary
+              : theme.colorScheme.onSurface);
     } else {
-      return iconTheme?.iconUnselectedColor ?? _theme.iconTheme.color;
+      return iconTheme?.iconUnselectedColor ??
+          (isDarkMode
+              ? theme.colorScheme.onSurface.withAlpha(150)
+              : theme.colorScheme.onSurface);
     }
   }
 
-  Color _getDisableToggleColor() => iconTheme?.disabledIconColor ?? _theme.disabledColor;
+  Color _getDisableToggleColor(bool isDarkMode, ThemeData theme) {
+    return iconTheme?.disabledIconColor ?? theme.disabledColor;
+  }
 
-  Color _getFillColor() {
+  Color _getFillColor(bool isDarkMode, ThemeData theme) {
     if (isToggled == true) {
-      return iconTheme?.iconSelectedFillColor ?? Theme.of(context).primaryColor;
+      return iconTheme?.iconSelectedFillColor ??
+          (isDarkMode ? theme.primaryColorDark : theme.primaryColor);
     } else {
-      return iconTheme?.iconUnselectedFillColor ?? _theme.canvasColor;
+      return iconTheme?.iconUnselectedFillColor ??
+          (isDarkMode ? theme.canvasColor : theme.colorScheme.background);
     }
   }
 
-  Color _getDisabledFillColor() => iconTheme?.disabledIconFillColor ?? (fillColor ?? _theme.canvasColor);
+  Color _getDisabledFillColor(ThemeData theme) {
+    return iconTheme?.disabledIconFillColor ?? theme.canvasColor;
+  }
 }
